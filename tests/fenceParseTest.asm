3bdcc8c232db1f6870a41b5e59371091961f44b8
yokhor <yokhor06@mail.ru>
2024-12-09 19:47:08 +0300

    Update tests

---
+++ tests/fenceParseTest.asm
@@ -0,0 +1,410 @@
+fence iorw , io
+fence iorw
+fence r, w
+fence 10, 15
+fence 11
+fence 0
+fence 15, 11
+fence iorw, iorw
+fence    o ,      io
+fence o,o
+fence i,i
+fence r,r
+fence w,w
+fence w,iorw
+fence wior
+fence io,rw
+fence rwi, ior
+fence
+
+
+
+
+
+
+
+
+
+
+
+rw
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+fence iorw
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+, 13
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+fence 15
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+fence 14
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+fence rwio
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+,
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+15
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+fence 3, io
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+fence rio, rio
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+fence                                                                                                                  ior,14
+
+
+
+
+
+
+
+
+
+fence 14,15
+fence iorw,0
+fence 0,0
+
+
+fence rio,9
+fence 9,riow
+fence 5,w
+
+
+
+
+
+
+
+
+
+
+
+
+
+fence 0,ior
+fence 0, ior
+fence 0 ,ior
+fence 0 , ior
+fence i , or
+fence orw  
+
+
+
+,
+
+
+i
+
+
+fence 15,15
+fence 12, iorw
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+fence 1,1
